8781f452641ae706150843c149acf71e4d8a2732
MikaGaete <mikagaete3@gmail.com>
2022-11-10 21:26:15 -0300

    Arreglado bby

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require("dotenv");
-const userRoutes = require("./routes/user");
-const router = express.Router();
 const cors = require('cors');
+require('dotenv').config({path: '../.env'});
 
-dotenv.config();
+const Routes = require('./routes/user');
 
 const app = express();
+app.use(cors());
 
 //middleware
 app.use(express.json());
-app.use('/api', userRoutes);
-app.use(cors());
+app.use('/api', Routes);
 
-
-//routes
-app.get('/', (req,res) => {
-    res.send('Welcome to my API');
-})
-
+app.get('/', (req,res)=>{
+    res.send('hello!');
+});
 
 //mongodb connection
 mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log('connected to MONGODB atlas'))
     .catch((error)=>console.log(error));
 
-app.listen(27017, () => console.log('server listening on port 27017'));
+app.listen(27017, () => console.log('listening in port 27017'));
