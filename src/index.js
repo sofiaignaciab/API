79e779c972a3d3c73cf9b83440b65477d83243aa
MikaGaete <mikael.gaete@mail.udp.cl>
2022-12-15 19:22:26 -0300

    API Final

--- src/index.js
+++ src/index.js
@@ -3,23 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
 require('dotenv').config();
-
-
 const Routes = require('./routes/user');
-
 const app = express();
+
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
     origin: "http://localhost:3000",
 }));
-
-//middleware
 app.use(express.json());
 app.use('/api', Routes);
-
-
-//mongodb connection
 mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log('connected to MONGODB atlas'))
     .catch((error)=>console.log(error));
